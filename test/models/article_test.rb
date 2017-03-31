f5c160308a123f2ae4d37ffa680d389bf178851d
Stefanie Moy <ssmores@hotmail.com>
2017-03-31 23:43:07 +0000

    Created test that should raise a NameError but pass the test case.

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,13 +1,20 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+    test "the truth" do
+        assert true
+    end
 
-  test "should not save article without title" do
-    article = Article.new
-    assert_not article.save, "Saved article without a title."
-  end
+    test "should not save article without title" do
+        article = Article.new
+        assert_not article.save, "Saved article without a title."
+    end
 
+    test "should report error" do
+        # some_undefined_variable is not defined anywhere, so this should raise an error
+        assert_raises(NameError) do
+            # since we assert that it raises a NameError, this test will now pass
+            some_undefined_variable
+        end
+    end
 end
